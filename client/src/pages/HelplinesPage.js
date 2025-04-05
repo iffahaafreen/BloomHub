736dfcac4c5c2ba572094d09cbb8bd4b413b7024
Iffah Aafreen <159646529+iffahaafreen@users.noreply.github.com>
2025-04-05 14:16:40 +0530

    Update HelplinesPage.js

--- client/src/pages/HelplinesPage.js
+++ client/src/pages/HelplinesPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import helplineData from "..\\client\\src\\helpline.json"; // Ensure correct path
+import helplineData from "../client/src/helpline.json"; 
 
 const HelplinesPage = () => {
   const [helplines, setHelplines] = useState([]);
